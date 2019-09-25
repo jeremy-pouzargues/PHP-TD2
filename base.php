5f71d0234322df1506d47bd1a54f66769fa46f75
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-09-25 16:08:07 +0200

    essai de la base de donnée

--- base.php
+++ base.php
@@ -6,6 +6,18 @@
     mysqli_select_db($dbLink,"jeremy-pouzargues_db")
         or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
 
+
     $query = 'SELECT id, email, date FROM user';
-    echo "$query";
+
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur de requête<br/>';
+        //Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        //Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+
+
 ?>
