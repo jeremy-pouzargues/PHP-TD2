8880b706b50be8bb4dbb7832a054340587852633
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-09-25 14:35:24 +0200

    test formulaire

--- formulaire.php
+++ formulaire.php
@@ -6,22 +6,22 @@
 <?php
     start_page('Formulaire');
 
-    echo '<input type="text" name="Id" placeholder="Identifiant" />
-          <input type="radio" name="CivH" placeholder="Homme"/>
-          <input type="radio" name="CivF" placeholder="Femme"/>
-          <input type="text" name="Mail" placeholder="E-Mail"/>
-          <input type="password" name="Mdp" placeholder="Mot de passe"/>
-          <input type="password" name="CMdp" placeholder="Verification du mot de passe"/>
-          <input type="text" name="Tel" placeholder="Téléphone"/>
+    echo '<input type="text" name="Id" placeholder="Identifiant" /> <br/>
+          <input type="radio" name="CivH" placeholder="Homme"/> <br/>
+          <input type="radio" name="CivF" placeholder="Femme"/> <br/>
+          <input type="text" name="Mail" placeholder="E-Mail"/> <br/>
+          <input type="password" name="Mdp" placeholder="Mot de passe"/> <br/>
+          <input type="password" name="CMdp" placeholder="Verification du mot de passe"/> <br/>
+          <input type="text" name="Tel" placeholder="Téléphone"/> <br/>
           <select name="Pays">
             <option value="Allemagne"/> Allemagne </option>
             <option value="Etats-Unis"/> Etats-Unis </option>
             <option value="France"/> France </option>
             <option value="Italie"/> Italie </option>
             <option value="Royaume-Uni"/> Royaume-Uni </option>
-          </select>
-          <input type="checkbox" name="CG" placeholder="Conditions Générales"/>
-          <input type="submit" name="submit" value="Envoyer"/>';
+          </select> <br/>
+          <input type="checkbox" name="CG" placeholder="Conditions Générales"/> <br/>
+          <input type="submit" name="submit" value="Envoyer"/> <br/>';
 
 
 
