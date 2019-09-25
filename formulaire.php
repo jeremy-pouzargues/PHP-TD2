f162f09d0b54e743b874a23779050950983f4cc0
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-09-25 14:20:17 +0200

    test formulaire

--- formulaire.php
+++ formulaire.php
@@ -5,6 +5,24 @@
 
 <?php
     start_page('Formulaire');
-    phpinfo();
+
+    echo '<input type="text" value="Identifiant"/>
+          <input type="radio" value="Civilité"/>
+          <input type="text" value="Mail"/>
+          <input type="password" value="Mdp"/>
+          <input type="password" value="Confirmer Mdp"/>
+          <input type="text" value="Téléphone"/>
+          <select name="Pays">
+            <option value="Allemagne"/> France </option>
+            <option value="Etats-Unis"/> France </option>
+            <option value="France"/> France </option>
+            <option value="Italie"/> France </option>
+            <option value="Royaume-Uni"/> France </option>
+          </select>
+          <input type="checkbox" value="CG"/>
+          <input type="submit"/>';
+
+
+
     end_page();
 ?>
