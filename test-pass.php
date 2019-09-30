076873013172af3e3d63f6b61b3aa8dfc8ab16df
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-09-30 08:44:03 +0200

    Mot de passe dans le code

--- test-pass.php
+++ test-pass.php
@@ -1,10 +1,11 @@
 <?php
+    include 'utils.inc.php';
     $log = $_POST['Log'];
     $pwd = $_POST['Pwd'];
     if ($pwd == 'jul')
         header('Location: page2.php');
     else
-        start_page('erreur');
+        start_page('Erreur');
 
     echo '<hr/><br/><strong>Erreur</strong><br/><br/><hr/>';
 
