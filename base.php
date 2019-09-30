11b3dc21e02fda50ad3d4e87fbdf545ecade2fc5
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-09-30 09:36:09 +0200

    mixer mot de passe et base de donnée

--- base.php
+++ base.php
@@ -16,6 +16,11 @@
         $query = 'SELECT MDP AS password FROM `user` WHERE IDENT = \'' . $log . '\'';
 
         if (!($dbResult = mysqli_query($dbLink, $query))) {
+            echo 'Erreur de requête<br/>';
+            //Affiche le type d'erreur.
+            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+            //Affiche la requête envoyée.
+            echo 'Requête : ' . $query . '<br/>';
             exit();
         }
 
